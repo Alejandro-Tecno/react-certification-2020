713f5cb824d0ce66e23b114e81e500a7ef44f771
alex <alejandro_n@outlook.com>
2021-03-18 18:46:08 -0600

    Not Found modified

--- src/pages/NotFound/NotFound.page.jsx
+++ src/pages/NotFound/NotFound.page.jsx
@@ -1,20 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import styled from "styled-components"
 
 
 
 function NotFoundPage() {
   return (
-    <section className="not-found">
+    <StyledNotFound>
+      <h1>Jhonny is lost :(</h1>
       <Link to="/" className="home-link">
-        home
+      <button type="button">
+      Get me back to Home
+     </button>
       </Link>
       <img src="404.gif" alt="page not found" />
-    </section>
+    </StyledNotFound>
   );
 }
 
+const StyledNotFound = styled.section`
+display: flex;
+flex-direction:column;
+align-items:center;
+h1{
+  font-size: 4rem;
+}
+button{
+  background: #a99cee;
+  height: 3rem;
+  font-size: 2rem;
+  border-radius: 10px;
+  &:hover{
+    background: #baaef5;
+    transform: scale(1.02);
+  }
+}
+`
 
 
 export default NotFoundPage;
